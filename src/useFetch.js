3587aec8429274ec621b0a56ac719e4d52dc9115
fabrod <fabrod035@gmail.com>
2021-12-28 09:24:29 -0500

    second commit

--- src/useFetch.js
+++ src/useFetch.js
@@ -27,7 +27,7 @@ const useFetch = (url) => {
            setIsPending(false)
            setError(err.message)
          })
-        }, 3000)
+        }, 1000)
        },[url])
        return {data, isPending, error}
 }
